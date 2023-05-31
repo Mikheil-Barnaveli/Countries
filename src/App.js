822d78fd011d39873b63ba6a4df87a4fd9a7e8a9
Mikheil-Barnaveli <barnavelimishiko@gmail.com>
2023-05-31 17:24:37 +0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
+import Header from './Header/Header';
+import SearchFilter from './SearchSection/SearchFilter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <SearchFilter/>
+    <div style={{width:"100%", backgroundColor:"red", minHeight:"70dvh", display:"flex", alignItems:"center", justifyContent:"center", flexWrap:"wrap", gap:"2rem"}}>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
+      <div style={{width:"20rem", height:'30rem', backgroundColor:"black", marginBlock:"1rem", color:"white"}}>CARD</div>
     </div>
+    </>
   );
 }
 
